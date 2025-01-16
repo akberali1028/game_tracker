a220629a54a1744af5881a21c8aaec0c95d4593c
akberali1028 <102252606+akberali1028@users.noreply.github.com>
2025-01-16 18:17:22 +0530

    gametracker.py

--- gametracker.py
+++ gametracker.py
@@ -2,18 +2,15 @@ import psutil
 from datetime import datetime
 import time
 import json
-from pynput.mouse import Listener as Listener 
-
+import threading
 import datetime
 from datetime import date
+import AFK_timer
 
 AFK_TIMEOUT = 5
-last_mouse_activity = time.time()
-is_afk = False
-afk_start_time = 0
-total_afk_time = 0
 game_on = False
-LOG_File = "game_sessions.json"
+LOG_File = "H:\\Work.exe\\M SHI\\game_sessions.json"
+total_afk_time=0
 
 fortnite_process="FortniteClient-Win64-Shipping.exe"
 valorant_process="VALORANT-Win64-Shipping.exe"
@@ -22,53 +19,36 @@ test_process="NZXT CAM.exe"
 
 all_processes = frozenset({fortnite_process,valorant_process,finals_process,test_process})
 
-def on_move(x, y):
-    global last_mouse_activity
-    last_mouse_activity = time.time()
-
-def on_click(x, y, button, pressed):
-    global last_mouse_activity
-    last_mouse_activity = time.time()
-
-# Function to check AFK status
-def check_afk():
-    global last_mouse_activity
-    return (time.time() - last_mouse_activity) > AFK_TIMEOUT
-
-# AFK tracking loop
-def track_afk():
-    global is_afk, afk_start_time, total_afk_time
-    while True:
-        if check_afk():
-            if not is_afk:  # User just became AFK
-                print("User is AFK")
-                afk_start_time = time.time()
-                is_afk = True
-        else:
-            if is_afk:  # User just came back
-                afk_end_time = time.time()
-                afk_duration = afk_end_time - afk_start_time
-                total_afk_time += afk_duration
-                print(f"User is back. AFK duration: {afk_duration:.2f} seconds")
-                is_afk = False
-            
-        time.sleep(1)   
-
 def is_game_running(game_name):
-    
     for process in psutil.process_iter():
         if process.name() == game_name:
             return True
     return False
     
+def inactive():
+    while game_on == True:
+        #afk code
+        time.sleep(3)
+        if game_on==False:
+            print("done")
+            break
+
 def time_game(game_name):
+    global game_on
     if is_game_running(game_name)==False:
         print("No game is running")
     else:
         start_time = time.time()
+        game_on=True
+        time.sleep(3)
+        t = threading.Thread(target=inactive)
+        t.start()
         print("game started")
         while(is_game_running(game_name)==True):
-            end_time = time.time()        
+            
+
+            end_time = time.time()      
+        game_on=False
         played = end_time-start_time
         print(f"Session was on for: {played:.2f} seconds")
         time_played= played-total_afk_time
